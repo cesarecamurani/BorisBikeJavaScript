7fa0502031ccbae8b47c24272b7fec9b47843731
cesare camurani <cesare.camurani@gmail.com>
2019-01-11 14:54:05 +0000

    Raise errors when station is empty or full

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -3,10 +3,33 @@ describe( 'DockingStation', function() {
 
   beforeEach(function() {
     station = new DockingStation;
+    bike = jasmine.createSpy('bike');
+  });
+
+  it('sets an empty rack', function() {
+    expect(station.bikes()).toEqual([]);
+  });
+
+  it('sets the capacity to 20', function() {
+    expect(station.capacity()).toEqual(20);
   });
 
   it('creates a new docking station', function() {
     expect(station).toBeDefined();
   });
+
+  it('does not release a bike if station is empty', function() {
+    expect(function() { station.release() }).toThrowError('cannot release a bike from an empty station');
+  });
+
+  it('does not dock a bike if station is full', function() {
+    expect(function() { station.dock(bike) }).toThrowError('cannot dock a bike to a full station');
+  });
+
+  it('docks a bike to the station', function() {
+    station.dock(bike);
+    expect(station.bikes()).toEqual([bike]);
+  });
+
 
 });
--- src/DockingStation.js
+++ src/DockingStation.js
@@ -1,1 +1,26 @@
-function DockingStation () {};
+function DockingStation () {
+  this.rack = [];
+  this.defaultCapacity = 20;
+};
+
+DockingStation.prototype.bikes = function() {
+  return this.rack;
+};
+
+DockingStation.prototype.capacity = function () {
+  return this.defaultCapacity;
+};
+
+DockingStation.prototype.release = function() {
+  if(this.rack.length === 0) {
+    throw new Error('cannot release a bike from an empty station');
+  };
+};
+
+DockingStation.prototype.dock = function(bike) {
+  if(this.rack.length === this.defaultCapacity) {
+    throw new Error('cannot dock a bike to a full station');
+  } else {
+    this.rack.push(bike);
+  }
+};
